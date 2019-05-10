1a98e5df1d533694066da185fa77c9cd8eb37451
xusitao07 <xusitao@gmail.com>
2019-05-10 15:28:05 +0800

    dddd

---
+++ TestCase/test_apptest.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+import unittest
+from Logs.log import log1
+from Common.Base_test import webrequests
+import time
+import unittest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import sys, traceback
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from appium.webdriver.webelement import WebElement
+from uitool import uiTool
+import Android_data,Android_data2,Android_data3
+from appium import webdriver
+
+
+
+
